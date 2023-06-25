575367052a9b7a60144b31beac2f3e809ed7ff76
cjephuneh <calebjephuneh@gmail.com>
2023-06-25 04:17:38 +0300

    updates

--- besides-nairobi-client/app/page.js
+++ besides-nairobi-client/app/page.js
@@ -1,8 +1,12 @@
 import React from "react"
 import Navbar from "@/components/sidebar"
+import Footer from "@/components/footer"
 
 export default function Home() {
   return (
+    <>
       <Navbar/>
+      <Footer />
+      </>
   )
 }
--- besides-nairobi-client/components/sidebar.jsx
+++ besides-nairobi-client/components/sidebar.jsx
@@ -7,13 +7,14 @@ import Image from 'next/image';
  const Navbar = () => {
   return (
     <nav className="flex items-center justify-between space-x-8 flex-wrap bg-white shadow-lg p-6">
-      <div className="flex items-center flex-shrink-0 mr-6">
+      <div className="flex items-center flex-shrink-0 mr-[450px]">
         
         <Image
           src={Logo}
           alt='logo'
-          width={50}
-          height={50}
+          width={130}
+          height={130}
+          // className='mr-[500px]'
         />
         
       </div>
@@ -22,7 +23,7 @@ import Image from 'next/image';
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0zM0 9h20v2H0zM0 15h20v2H0z"/></svg>
         </button>
       </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto ">
         <div className="text-sm lg:flex-grow">
           <Link href="/">
             <h1 className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-red-500  mr-4">
