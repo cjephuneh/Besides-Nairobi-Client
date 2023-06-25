b277e95d896d2e0e2831a50aee3c8732b1b3eaf5
cjephuneh <calebjephuneh@gmail.com>
2023-06-25 11:20:01 +0300

    updates

--- besides-nairobi-client/app/page.js
+++ besides-nairobi-client/app/page.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Navbar from "@/components/sidebar"
 import Footer from "@/components/footer"
+import Landing  from "../components/Home"
 
 export default function Home() {
   return (
     <>
       <Navbar/>
       {/* <Footer /> */}
+      <Landing/>
       </>
   )
 }
--- besides-nairobi-client/components/Home.jsx
+++ besides-nairobi-client/components/Home.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import Head from 'next/head';
 
 function Home() {
   return (
-    <div>
-      
+    <>
+    <div className="h-screen bg-cover bg-center" style={{ backgroundImage: "url('/assets/images.jpeg')", height: "50vh" }}>
+      <Head>
+        <title>Welcome to Bsides Nairobi</title>
+      </Head>
+      <div className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-4xl font-bold text-white text-center">Welcome to Bsides Nairobi</h1>
+      </div>
     </div>
-  )
+
+    <div className='bg-gray-100 p-8'>
+  <h1 className='text-green-500 font-bold text-4xl mb-8'>2023 Bsides Nairobi</h1>
+  <span className='text-lg text-gray-900 font-bold mb-4'>
+    ​Watch this space and our socials for updates on this year's conference.
+  </span>
+  <span className='text-lg text-gray-900 font-bold mb-4 block'>
+    Dates:
+    <span className='ml-2 font-normal'>8-10th September, 2023</span>
+  </span>
+  <span className='text-lg text-gray-900 font-bold mb-4 block'>
+    Sponsorship:
+    <a href='#' className='ml-2 font-normal text-blue-500 hover:underline'>Interested in Supporting and Sponsoring check out our Sponsorship Packages</a>
+  </span>
+  <span className='text-lg text-gray-900 font-bold mb-4 block'>
+    Volunteering at BSides:
+    <a href='#' className='ml-2 font-normal text-blue-500 hover:underline'>Volunteering Opportunities are now open</a>
+  </span>
+  <span className='text-lg text-gray-900 font-bold mb-4 block'>
+    Diversity and Inclusion / Financial Aid Scholarship:
+    <a href='#' className='ml-2 font-normal text-blue-500 hover:underline'>Apply Now</a>
+  </span>
+  <span className='text-lg text-gray-900 font-bold mb-4 block'>
+    CFP and CFT:
+    <span className='ml-2 font-normal'>now open</span>
+  </span>
+  <span className='text-lg text-gray-900 font-bold mb-4 block'>
+    Tickets:
+    <span className='ml-2 font-normal'>Announcements coming soon</span>
+  </span>
+</div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
